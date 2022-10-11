03453ba0e2ea54f9e823b98b2f7529b4196b2552
syafiqnazim <56761460+syafiqnazim@users.noreply.github.com>
2022-10-11 13:42:20 +0800

    declare only one variable and use it on each function

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-let num1 = 8
-let num2 = 2
-document.getElementById("num1-el").textContent = num1
-document.getElementById("num2-el").textContent = num2
+let num1 = 8;
+let num2 = 2;
+let result = 0;
+document.getElementById("num1-el").textContent = num1;
+document.getElementById("num2-el").textContent = num2;
 
 // Create four functions: add(), subtract(), divide(), multiply()
 // Call the correct function when the user clicks on one of the buttons
@@ -14,22 +15,22 @@ document.getElementById("num2-el").textContent = num2
 let sumEl = document.getElementById("sum-el")
 
 function add() {
-    let result = num1 + num2
+    result = num1 + num2
     sumEl.textContent = "Sum: " + result;
 }
 
 function subtract() {
-    let result = num1 - num2
+    result = num1 - num2
     sumEl.textContent = "Sum: " + result;
 }
 
 function multiply() {
-    let result = num1 * num2
+    result = num1 * num2
     sumEl.textContent = "Sum: " + result;
 }
 
 function divide() {
-    let result = num1 / num2
+    result = num1 / num2
     sumEl.textContent = "Sum: " + result;
 }
 
